e460aa9c2a3e118d8afa25f58a0837fd7cbb639f
P Reijerkerk <preijerkerk@gmail.com>
2018-12-17 23:59:34 -0500

    Added in basic layout into app.js removed all css from each component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React, { Component } from 'react';
-
-import './App.css';
+import monsters from './monsters.json';
+import Wrapper from './components/wrapper/wrapper';
+import Navbar from './components/navbar/navbar';
+import Header from './components/header/header';
+import Scoreboard from './components/scoreboard/scoreboard';
+import ImageCard from './components/imageCard/imageCard';
+import GameOver from './components/gameover/gameover';
 
 class App extends Component {
-  
+    state = {
+      msg: 'Click any monster to begin',
+      score: 0,
+      highScore: 0,
+      monsters: monsters,
+      selected: [],
+      gameover: false,
+      countdown: '',
+    }
+
+
+render() {
+    return (
+        <Wrapper>
+          <Navbar />
+          <Header />
+          <Scoreboard 
+            msg={this.state.msg}
+            score={this.state.score}
+            highScore={this.state.highScore}
+          />
+          <div className="container">  
+          { 
+            this.state.monsters.map(monster => (
+              <ImageCard 
+                key={monster.id}
+                id={monster.id}
+                img_url={monster.img_url}
+                
+              />
+            ))
+          };
+          </div>
+        </Wrapper>
+      )
+    }  
 }
+ 
+
+
+    
+
 
 export default App;
