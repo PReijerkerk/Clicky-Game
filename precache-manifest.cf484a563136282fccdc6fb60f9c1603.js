580c08a4b8487bb12b505b59aa1ff6b4fb831df3
P Reijerkerk <preijerkerk@gmail.com>
2018-12-19 18:33:40 -0500

    Updates

---
+++ precache-manifest.cf484a563136282fccdc6fb60f9c1603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fbbbddfaff1ee5c40e9d16e4b31ffad",
+    "url": "/Clicky-Game/static/media/winner.9fbbbddf.jpg"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "9610cdb5835dcfd5f76d",
+    "url": "/Clicky-Game/static/js/main.9610cdb5.chunk.js"
+  },
+  {
+    "revision": "3f77c3e24b2b95d0bdcf",
+    "url": "/Clicky-Game/static/js/1.3f77c3e2.chunk.js"
+  },
+  {
+    "revision": "9610cdb5835dcfd5f76d",
+    "url": "/Clicky-Game/static/css/main.f9c3508c.chunk.css"
+  },
+  {
+    "revision": "77e6fa85364efe4118ad1ef1604c3e89",
+    "url": "/Clicky-Game/index.html"
+  }
+];
