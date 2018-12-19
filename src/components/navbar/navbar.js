eaadd7daf0f6161d3431174cdf1c00f0beba7820
P Reijerkerk <preijerkerk@gmail.com>
2018-12-19 18:43:33 -0500

    fixed bug for deployed version

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,7 +4,7 @@ import './navbar.css';
 const Navbar = () => (
     <nav className="navbar mb-1 navigation">
       <div className="navbar-header">
-        <a className="navbar-brand" href="/">Scooby-Doo Clicky-Game!</a>
+        <a className="navbar-brand" href="/Clicky-Game">Scooby-Doo Clicky-Game!</a>
       </div>
       <ul className="nav navbar-nav">
         <a class="github" href="https://github.com/PReijerkerk/Clicky-Game">GitHub Repo</a>
